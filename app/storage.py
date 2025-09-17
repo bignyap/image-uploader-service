26a3cf329618e7136dc3ae020b240eb68579bc5d
bignya <bignyaranjan@gmail.com>
2025-09-17 21:07:45 +0530

    Added the S3Service class

---
+++ app/storage.py
@@ -0,0 +1,41 @@
+import boto3
+from typing import Optional, Dict, Any
+from .settings import settings
+import logging
+
+log = logging.getLogger(__name__)
+
+class S3Service():
+    def __init__(self):
+        session = boto3.session.Session(region_name=settings.aws_region)
+        kwargs = {
+            "aws_access_key_id": settings.aws_access_key_id,
+            "aws_secret_access_key": settings.aws_secret_access_key,
+        }
+        if settings.aws_endpoint_url:
+            kwargs["endpoint_url"] = settings.aws_endpoint_url
+
+        self.client = session.client("s3", **kwargs)
+        log.info("Initialized S3 client")
+
+    def upload(self, fileobj, key: str, content_type: str):
+        self.client.upload_fileobj(
+            Fileobj=fileobj,
+            Bucket=settings.s3_bucket,
+            Key=key,
+            ExtraArgs={"ContentType": content_type},
+        )
+        log.debug("Uploaded %s to s3://%s/%s", key, settings.s3_bucket, key)
+
+
+    def generate_presigned_url(self, key: str, expires_in: Optional[int] = None) -> str:
+        expires = expires_in if expires_in is not None else settings.presign_expire_seconds
+        return self.client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": settings.s3_bucket, "Key": key},
+            ExpiresIn=expires,
+        )
+    
+    def delete(self, key: str):
+        self.client.delete_object(Bucket=settings.s3_bucket, Key=key)
+        log.debug("Deleted s3://%s/%s", settings.s3_bucket, key)
